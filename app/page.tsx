c1a210fdffa978902de8e702db7f73fa32f7fcaa
perryliu540540749 <gliu0050@uni.sydney.edu.au>
2025-04-11 00:03:59 +1000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to Strata Manager</h1>
+      <p>Manage your condo or apartment easily!</p>
+      <p>這是你的物業管理網站！</p>
+    </div>
+  );
+}
