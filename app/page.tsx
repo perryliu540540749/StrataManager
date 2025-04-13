d164fba1ac438636865e4f42f5671d92a81a47c1
perryliu540540749 <gliu0050@uni.sydney.edu.au>
2025-04-13 23:36:08 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ export default function Home() {
       <p>Manage your condo or apartment easily!</p>
       <p>這是你的物業管理網站！</p>
       <p>Contact us at {process.env.MANAGER_EMAIL}</p>
-      <img src="/condo.jpg" alt="Condo" width="300" />
-      <br />
-      <a href="/rules.pdf" target="_blank">Download Rules</a>
     </div>
   );
 }
